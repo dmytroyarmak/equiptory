02e665ff303ca75e48bcbc7e2dede49942902da1
Dima <0979029562@mail.ru>
2012-09-17 13:01:04 +0300

    link equipment and state

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,5 +1,6 @@
 class Equipment < ActiveRecord::Base
-  attr_accessible :name, :price, :location_id, :type_id
+  attr_accessible :name, :price, :location_id, :type_id, :state_id
   belongs_to :location
   belongs_to :type
+  belongs_to :state
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120917095337) do
+ActiveRecord::Schema.define(:version => 20120917095940) do
 
   create_table "equipment", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20120917095337) do
     t.datetime "updated_at",  :null => false
     t.integer  "location_id"
     t.integer  "type_id"
+    t.integer  "state_id"
   end
 
   create_table "locations", :force => true do |t|
