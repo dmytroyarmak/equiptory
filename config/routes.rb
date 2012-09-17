4c37bbbac4ea6e33b5233a63f19f2bd001fce9fa
Dima <0979029562@mail.ru>
2012-09-17 11:27:19 +0300

    change index to equipment list

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ Equiptory::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'equipment#index'
 
   # See how all your routes lay out with "rake routes"
 
