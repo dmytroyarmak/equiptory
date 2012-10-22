3f53610a0f91e7370fc1572254044caa3b6b139b
Dima <0979029562@mail.ru>
2012-10-22 10:09:20 +0300

    add nested relationship

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,6 +1,7 @@
 class Equipment < ActiveRecord::Base
-  attr_accessible :name, :price, :location_id, :type_id, :state_id
+  attr_accessible :name, :price, :location_id, :type_id, :state_id, :attribute_values_attributes
   has_many :attribute_values
+  accepts_nested_attributes_for :attribute_values, allow_destroy: true
   belongs_to :location
   belongs_to :type
   belongs_to :state
