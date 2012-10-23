a9c42d0ded7eb3225b25eed60d8ca41cdee8a387
Dima <0979029562@mail.ru>
2012-10-23 21:36:49 +0300

    some validations and error fix

--- app/models/type.rb
+++ app/models/type.rb
@@ -1,7 +1,7 @@
 class Type < ActiveRecord::Base
   attr_accessible :name, :attribute_types_attributes
   has_many :equipment
-  has_many :attribute_types
+  has_many :attribute_types, :dependent => :destroy
   accepts_nested_attributes_for :attribute_types, allow_destroy: true
   validates :name, :presence => true
 end
